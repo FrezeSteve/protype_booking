a5cc3b3fc8213df897be27131c5ce07c567723de
steve <stevefrisson@gmail.com>
2021-04-14 08:12:54 +0300

    the first design of the footer

--- src/Containers/Footer.js
+++ src/Containers/Footer.js
@@ -6,7 +6,7 @@ const Example = (props) => {
             {props.children}
             <div style={{ bottom: "0", backgroundColor: "#183952!important" }}>
                 <div className="container pt-sm-5">
-                    <h1>Hello World</h1>
+                    <h1>Footer</h1>
                 </div>
             </div>
         </>
